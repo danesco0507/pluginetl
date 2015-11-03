517e07fc332673932f599b636d5afc9778fdc4d7
danesco0507 <danesco0507@gmail.com>
2015-11-03 16:18:18 -0500

    Se modifica el clusteres EJB para que genere los Clusters de visualizacion. Se deben general los plugins para el metamodelo de visualizacion.

--- src/clusterer/EJBClusterer.java
+++ src/clusterer/EJBClusterer.java
@@ -1,13 +1,24 @@
 package clusterer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import subkdm.kdmObjects.AbstractCodeElement;
 import subkdm.kdmObjects.CodeItem;
+import subkdm.kdmObjects.KdmObjectsFactory;
+import subkdm.kdmObjects.Module;
+import subkdm.kdmObjects.Package;
 import subkdm.kdmRelations.ClassLevelRelation;
 import subkdm.kdmRelations.TypeRelation;
+import visualizacionMetricas3.Elemento;
+import visualizacionMetricas3.representacion.Clase;
+import visualizacionMetricas3.representacion.Cluster;
+import visualizacionMetricas3.representacion.Paquete;
+import visualizacionMetricas3.representacion.RepresentacionFactory;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
@@ -15,12 +26,15 @@ import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 public class EJBClusterer {
 	
 	DirectedGraph<CodeItem, ClassLevelRelation> graph;
+	RepresentacionFactory factory;
 	
 	public EJBClusterer(){
 		graph = new DirectedSparseMultigraph<CodeItem, ClassLevelRelation>();
+		factory = RepresentacionFactory.eINSTANCE;
 	}
 	
 	public void constructGraph(Set<CodeItem> classes, Set<ClassLevelRelation> relations){
+
 		for(CodeItem c : classes){
 			graph.addVertex(c);
 		}
@@ -30,71 +44,84 @@ public class EJBClusterer {
 		}
 	}
 	
-	public Set<Set<CodeItem>> makeCluster(Set<CodeItem> ejbs, Set<CodeItem> entities){
+	public Paquete getOrCreatePackage(Cluster cluster, CodeItem codeItem){
+		Package pkg = (Package) codeItem.eContainer();
+		for(Elemento hijo : cluster.getHijos()){
+			if(hijo.getName().equals(pkg.getName())){
+				return (Paquete)hijo;
+			}
+		}
+		Paquete pq = factory.createPaquete();
+		pq.setName(pkg.getName());
 		
-		Set<Set<CodeItem>> firtsclusters = new HashSet<Set<CodeItem>>();
+		return pq;
+	}
+	
+	public Set<Cluster> makeCluster(Set<CodeItem> ejbs, Set<CodeItem> entities){
+		
+		Set<Cluster> firstclusters = new HashSet<Cluster>();
 		
 		DijkstraShortestPath<CodeItem, ClassLevelRelation> sp = 
 				new DijkstraShortestPath<CodeItem, ClassLevelRelation>(graph, true);
 		
-		Set<CodeItem> allNodes = new HashSet<CodeItem>(graph.getVertices());
-		
-		Set<Set<CodeItem>> allEjbHt = constructHeritageTree(ejbs);
 		Set<Set<CodeItem>> allEntityHt = constructHeritageTree(entities);
 		
-		for (Set<CodeItem> ejbHt : allEjbHt)
-		{
-			Set<CodeItem> cluster = new HashSet<CodeItem>();
-			cluster.addAll(ejbHt);
-			//ESTO PUEDE SER MUY LENTO
-			for(CodeItem ejbHtElem : ejbHt)
-			{		
+		
+		for(CodeItem ejb : ejbs){
+			Cluster mod = factory.createCluster();
+			mod.setName(ejb.getName());
+			
+			Set<CodeItem> elementos = new HashSet<CodeItem>();
+			
+			for(CodeItem elem : getHeritageTree(ejb)){
+				elementos.add(elem);
 				for(Set<CodeItem> entityHt : allEntityHt)
 				{
 					for(CodeItem entHtElem : entityHt)
 					{
-						if(!sp.getPath(ejbHtElem, entHtElem).isEmpty()){
-							cluster.addAll(entityHt);
-							for(ClassLevelRelation clr : sp.getPath(ejbHtElem, entHtElem)){
-								cluster.add(clr.getFrom());
-								cluster.add(clr.getTo());
+						if(!sp.getPath(elem, entHtElem).isEmpty()){
+							elementos.addAll(entityHt);
+							for(ClassLevelRelation clr : sp.getPath(elem, entHtElem)){
+								elementos.add(clr.getFrom());
+								elementos.add(clr.getTo());
 							}
 						}
 					}
 				}
 			}
-			firtsclusters.add(cluster);
-		}
-		
-		for(Set<CodeItem> ht : allEjbHt){
-			allNodes.removeAll(ht);
-		}
-		
-		Set<Set<CodeItem>> lastclusters = new HashSet<Set<CodeItem>>();
-		
-		for (Set<CodeItem> subCluster : firtsclusters){
-			Set<CodeItem> cluster = new HashSet<CodeItem>();
-			cluster.addAll(subCluster);
 			
-			for(CodeItem ci : subCluster){
+			List<CodeItem> allNodes = new ArrayList<CodeItem>(graph.getVertices());
+			allNodes.removeAll(elementos);
+			
+			Set<CodeItem> extras = new HashSet<CodeItem>();
+			for (CodeItem elem : elementos){
 				for(CodeItem item : allNodes)
 				{
-					for(ClassLevelRelation clr : sp.getPath(ci, item)){
-						cluster.add(clr.getFrom());
-						cluster.add(clr.getTo());
+					for(ClassLevelRelation clr : sp.getPath(elem, item)){
+						extras.add(clr.getFrom());
+						extras.add(clr.getTo());
 					}
-					
-					for(ClassLevelRelation clr : sp.getPath(item, ci)){
-						cluster.add(clr.getFrom());
-						cluster.add(clr.getTo());
+						
+					for(ClassLevelRelation clr : sp.getPath(item, elem)){
+						extras.add(clr.getFrom());
+						extras.add(clr.getTo());
 					}
 				}
 			}
 			
-			lastclusters.add(cluster);
+			elementos.addAll(extras);
+			
+			for(CodeItem elem : elementos){
+				Paquete pk = getOrCreatePackage(mod, elem);
+				Clase cl = factory.createClase();
+				cl.setName(elem.getName());
+				pk.getHijos().add(cl);
+				mod.getHijos().add(pk);
+			}
+			firstclusters.add(mod);
 		}
 		
-		return lastclusters;
+		return firstclusters;
 	}
 	
 	public Set<Set<CodeItem>> constructHeritageTree(Set<CodeItem> ejbs){
@@ -113,6 +140,7 @@ public class EJBClusterer {
 		
 		for(ClassLevelRelation clr: allclr){
 			if(isExtensionOrImplementation(clr)){
+				System.out.println("-----relación: " + clr.getFrom() + " a " + clr.getTo());
 				set.add(clr.getFrom());
 				set.addAll(getHeritageTree(clr.getTo()));
 			}
